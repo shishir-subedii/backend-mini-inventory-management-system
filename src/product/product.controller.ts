7ed312e0c66d3ebca8ff4fbe95a1d1bdc0667c19
Shishir <shishir16subedi@gmail.com>
2025-07-25 14:58:59 +0545

    Feat(Product): added product create and fetch routes

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { JwtAuthGuard } from 'src/common/auth/AuthGuard';
 import { Roles } from 'src/common/auth/AuthRoles';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { userPayloadType } from 'src/common/types/auth.types';
+import { productParamDto } from './dto/productParam.dto';
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
@@ -13,19 +15,50 @@ import { ApiBearerAuth } from '@nestjs/swagger';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ApiOperation({ summary: 'Create a new product' })
+  @ApiBody({
+    type: CreateProductDto,
+    description: 'Product details to create',
+  })
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  async create(
+    @Req() req: Request,
+    @Body() createProductDto: CreateProductDto
+  ) {
+    const user = req['user'] as userPayloadType;
+    const product = await this.productService.create(createProductDto, user);
+    return {
+      success: true,
+      data: product,
+      message: 'Product created successfully',
+    }
   }
 
+  @ApiOperation({ summary: 'Get all products for the user' })
   @Get()
-  findAll() {
-    return this.productService.findAll();
+  async findAll(
+    @Req() req: Request,
+  ) {
+    const user = req['user'] as userPayloadType;
+    const products = await this.productService.findAll(+user.id);
+    return {
+      success: true,
+      data: products,
+      message: 'Products retrieved successfully',
+    }
   }
 
+  @ApiOperation({ summary: 'Get a product by ID' })
+  @ApiParam({ name: 'id', type: Number, description: 'Product ID' })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+  async findOne(@Param('id') p: productParamDto, @Req() req: Request) {
+    const user = req['user'] as userPayloadType;
+    const product = await this.productService.findOne(+p.id, +user.id);
+    return {
+      success: true,
+      data: product,
+      message: 'Product retrieved successfully',
+    }
   }
 
   @Patch(':id')
